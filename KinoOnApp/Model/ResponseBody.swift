77d12290dba668ce909466edf40a986492dac552
Konstantin Pronin <sayfer97@yandex.ru>
2020-11-27 17:52:09 +0300

    fixed

--- KinoOnApp/Model/ResponseBody.swift
+++ KinoOnApp/Model/ResponseBody.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct ResponseBody<T: Decodable>: Decodable {
+    var status: Int
+    var body: T
+    
+    enum CodingKeys: String, CodingKey {
+        case status = "status"
+        case body = "body"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        status = try container.decode(Int.self, forKey: .status)
+        body = try container.decode(T.self, forKey: .body)
+    }
+}
